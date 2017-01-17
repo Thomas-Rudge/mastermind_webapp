8e05c5174bc0be4bec9df558a043f75628e0b0ca
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-01-17 20:53:22 +0000

    Working on AI
    Trying to get the AI to work. For some reason jQuery cant update the form input value :/

--- app.rb
+++ app.rb
@@ -28,13 +28,18 @@ end
 get "/ai" do
   @@game.reset if params[:replay] == "true"
 
-  guess = @@game.get_ai_response
-  puts "#{guess}"
+  guess = params[:guess]
+  guess = @@game.format_player_response(guess)
 
-  @@game.register_player_response(guess)
-  state = @@game.check_game_status
+  unless guess.nil? || guess.empty? || (@@game.guesses.include? guess)
+    @@game.register_player_response(guess)
+    sleep(1)
+  end
+
   table = @@game.generate_mastermind_table
+  state = @@game.check_game_status
 
+  guess = @@game.get_ai_response
 
   erb :game, :locals=>{:table=>table, :state=>state, :ai=>true, :guess=>guess.join}
 end
--- public/script.js
+++ public/script.js
@@ -1,28 +1,27 @@
 $(document).ready(function() {
   // When a user clicks an input colour, update the board
-  $('#select_red').click(function() { updateBoard('red') });
-  $('#select_orange').click(function() { updateBoard('orange') });
-  $('#select_yellow').click(function() { updateBoard('yellow') });
-  $('#select_blue').click(function() { updateBoard('blue') });
-  $('#select_green').click(function() { updateBoard('green') });
-  $('#select_purple').click(function() { updateBoard('purple') });
+  $('#select_red').click(function() { updateBoard('r') });
+  $('#select_orange').click(function() { updateBoard('o') });
+  $('#select_yellow').click(function() { updateBoard('y') });
+  $('#select_blue').click(function() { updateBoard('b') });
+  $('#select_green').click(function() { updateBoard('g') });
+  $('#select_purple').click(function() { updateBoard('p') });
   $('#retry').click(function() {  window.location.href = '/play?replay=true' });
   $('#home').click(function() { window.location.href = '/' });
 
   var updateBoard = function(colour) {
-    var cellColours = {red:'#CD3129',
-                       orange:'#CD7629',
-                       yellow:'#CDCA29',
-                       blue:'#27458A',
-                       purple:'#60228A',
-                       green:'#209F30'};
+    var cellColours = {r:'#CD3129',
+                       o:'#CD7629',
+                       y:'#CDCA29',
+                       b:'#27458A',
+                       p:'#60228A',
+                       g:'#209F30'};
 
     var userGuess = $('input[name="guess"]').val();
+
     userGuess += colour[0]
     $('input[name="guess"]').val(userGuess);
-    //$('#mastermind_table').find('.cell').first().css('background-color', 'white');
     var next_cell = $('#mastermind_table').find('.cell').first();
-    console.log(cellColours[colour]);
     next_cell.addClass('xcell').removeClass('cell');
     next_cell.css('background-color', cellColours[colour]);
 
@@ -31,5 +30,12 @@ $(document).ready(function() {
 
   var submitGuess = function(guess) {
     $('input[name="submit"]').click();
+  };
+
+  if (window.location.pathname === '/ai' && $('#guess_data') !== null) {
+    aiGuess = $('#guess_data').text();
+    console.log(aiGuess);
+
+
   };
 });
