7222be8ec30ff1d3034a08cef279f4273090a5c9
Thomas-Rudge <thomas.rudge85@gmail.com>
2017-01-19 14:09:04 +0000

    Optimised for handheld devices
    The table is now built dynamically by jQuery.

--- public/script.js
+++ public/script.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
   $('#retry').click(function() { retryGame(); return false; });
   $('#home').click(function() { window.location.href = '/' });
   $('#codemaker_button').click(function() { startCodemaker() });
+  $('#codebreaker_button').click(function() { startCodebreaker() });
   $('#code_input').click(function() { showCodeTip(); return false; });
 
   var retryGame = function(user) {
@@ -27,6 +28,37 @@ $(document).ready(function() {
   var showCodeTip = function() {
     console.log('Triggered');
     $('#code_tip').slideDown();
+  };
+
+  var getAvailableResolution = function() {
+    return [$(window).width(), $(window).height()];
+  };
+
+  var setBoardSize = function() {
+    var res = getAvailableResolution();
+
+    var cellxy  = res[1] / 15;
+    var pegxy   = Math.round((cellxy - 1) / 2);
+    var inputxy = Math.round(cellxy / 1.16);
+    cellxy  = Math.round(cellxy);
+
+    var table_width = (cellxy * 4) + (pegxy * 2) + 12;
+
+    cellxy      = cellxy.toString() + 'px';
+    pegxy       = pegxy.toString() + 'px';
+    inputxy     = inputxy.toString() + 'px';
+    table_width = table_width.toString() + 'px';
+
+    $('#mastermind_container').css('width', table_width);
+    $('.button_container').css('width', table_width);
+    $('.game_cell').css( {'width': cellxy, 'height': cellxy} );
+    $('.peg').css( {'width': pegxy, 'height': pegxy} );
+    $('.input_cell').css( {'width': inputxy, 'height': inputxy} );
+  };
+
+  var startCodebreaker = function() {
+    user_uri = '/play?replay=true';
+    window.location.href = user_uri;
   };
 
   var startCodemaker = function() {
@@ -75,6 +107,8 @@ $(document).ready(function() {
   };
 
   if (window.location.pathname === '/ai' && $('#guess_data') !== null) {
+    setBoardSize();
+
     aiGuess = $('#guess_data').text();
 
     setTimeout(function(){
@@ -82,5 +116,7 @@ $(document).ready(function() {
         updateBoard(c);
       };
     }, 1200);
+  } else if (window.location.pathname === '/play') {
+    setBoardSize();
   };
 });
